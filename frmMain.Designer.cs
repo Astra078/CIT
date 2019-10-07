05a40cab16854b2376ee66c614faf4ea58c432b6
Astra078 <54424091+Astra078@users.noreply.github.com>
2019-10-07 18:14:11 +1100

    Upload of Drag&Drop
    Upload of Drag&Drop work

---
+++ frmMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Assessment2DragDrop
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(394, 15);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drop an Image onto this Form";
+            this.label1.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(17, 38);
+            this.picBox.MaximumSize = new System.Drawing.Size(1920, 990);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(200, 200);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBox.TabIndex = 3;
+            this.picBox.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Drag & Drop";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picBox;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,61 @@
+﻿/**
+*** Author: Conor Lynch CIT185593
+*** Date: 07/10/2019
+*** IDE: Microsoft Visual Studio Enterprise 2017
+*** IDE Version: 15.9.6
+*** Software: Microsoft .NET Framework
+*** Software Version: 4.7.02558
+*** Platform: Microsoft Windows 8.1 64-bit
+*** Platform Version: 6.3.9600 Build 9600
+*** Project: Assessment02
+*** Objective: Creation of a drag and drop image display application 
+**/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Assessment2DragDrop
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            try
+            {
+                foreach (string File in fileName)
+                    picBox.Image = Image.FromFile(File);//
+            }
+            catch
+            {
+                MessageBox.Show("Not a valid image");
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.DragEnter += new DragEventHandler(frmMain_DragEnter);
+            this.DragDrop += new DragEventHandler(frmMain_DragDrop);
+        }
+    }
+}
