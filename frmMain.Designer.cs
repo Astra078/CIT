ed136f3039c2305756737ba37e6610574f481c1f
Astra078 <54424091+Astra078@users.noreply.github.com>
2019-10-07 18:17:21 +1100

    Upload 2D Graphics
    Upload 2D Graphics assessment critera

---
+++ frmMain.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Assessment2Graphics
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDrawHouse = new System.Windows.Forms.Button();
+            this.panDraw = new System.Windows.Forms.Panel();
+            this.btnDrawClear = new System.Windows.Forms.Button();
+            this.lblAbout = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 228);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDrawHouse
+            // 
+            this.btnDrawHouse.Location = new System.Drawing.Point(12, 51);
+            this.btnDrawHouse.Name = "btnDrawHouse";
+            this.btnDrawHouse.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawHouse.TabIndex = 1;
+            this.btnDrawHouse.Text = "House";
+            this.btnDrawHouse.UseVisualStyleBackColor = true;
+            this.btnDrawHouse.Click += new System.EventHandler(this.btnDrawHouse_Click);
+            // 
+            // panDraw
+            // 
+            this.panDraw.BackColor = System.Drawing.Color.LightGray;
+            this.panDraw.Location = new System.Drawing.Point(109, 51);
+            this.panDraw.Name = "panDraw";
+            this.panDraw.Size = new System.Drawing.Size(200, 200);
+            this.panDraw.TabIndex = 4;
+            // 
+            // btnDrawClear
+            // 
+            this.btnDrawClear.Location = new System.Drawing.Point(12, 80);
+            this.btnDrawClear.Name = "btnDrawClear";
+            this.btnDrawClear.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawClear.TabIndex = 5;
+            this.btnDrawClear.Text = "Clear";
+            this.btnDrawClear.UseVisualStyleBackColor = true;
+            this.btnDrawClear.Click += new System.EventHandler(this.btnDrawClear_Click);
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbout.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblAbout.Location = new System.Drawing.Point(12, 13);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(240, 25);
+            this.lblAbout.TabIndex = 6;
+            this.lblAbout.Text = "Conor Lynch - CIT185593";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 263);
+            this.Controls.Add(this.lblAbout);
+            this.Controls.Add(this.btnDrawClear);
+            this.Controls.Add(this.panDraw);
+            this.Controls.Add(this.btnDrawHouse);
+            this.Controls.Add(this.btnExit);
+            this.DoubleBuffered = true;
+            this.Name = "frmMain";
+            this.Text = "2D Graphics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDrawHouse;
+        private System.Windows.Forms.Panel panDraw;
+        private System.Windows.Forms.Button btnDrawClear;
+        private System.Windows.Forms.Label lblAbout;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,76 @@
+﻿/**
+*** Author: Conor Lynch CIT185593
+*** Date: 07/10/2019
+*** IDE: Microsoft Visual Studio Enterprise 2017
+*** IDE Version: 15.9.6
+*** Software: Microsoft .NET Framework
+*** Software Version: 4.7.02558
+*** Platform: Microsoft Windows 8.1 64-bit
+*** Platform Version: 6.3.9600 Build 9600
+*** Project: Assessment02
+*** Objective: Creation of a 2D graphics on a window form
+**/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Assessment2Graphics
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void house_Paint(object sender, PaintEventArgs e)
+        {
+            //First Paramater is Horizontal position
+            //Second Paramater is Vertical position
+            //Third Paramater is total width
+            //Forth Paramater is total height
+
+            e.Graphics.FillRectangle(Brushes.SkyBlue, 0, 0, 200, 200);//Background
+            e.Graphics.FillRectangle(Brushes.BurlyWood, 20, 50, 160, 130);//House
+            e.Graphics.DrawRectangle(Pens.Black, 20, 50, 160, 130);//House Outline
+            e.Graphics.FillRectangle(Brushes.Green, 0, 180, 200, 20);//Grass
+            Point[] roof = { new Point(5, 50), new Point(195, 50), new Point(100, 20) };//roof points
+            e.Graphics.FillPolygon(Brushes.Red, roof);//roof
+            e.Graphics.DrawPolygon(Pens.DarkRed, roof);//roof outline
+            e.Graphics.FillRectangle(Brushes.Black, 38, 78, 67, 102);//Door outline
+            e.Graphics.FillRectangle(Brushes.Moccasin, 40, 80, 63, 100);//Door
+            e.Graphics.FillEllipse(Brushes.Gray, new Rectangle(88, 122, 9, 9));//Door handle
+            e.Graphics.FillRectangle(Brushes.Moccasin, 120, 90, 50, 40);//Window
+            e.Graphics.DrawRectangle(Pens.Black, 120, 90, 50, 40);//Window Outline
+            e.Graphics.FillRectangle(Brushes.LightSkyBlue, 124, 94, 43, 33);//Window Class
+            e.Graphics.FillRectangle(Brushes.Moccasin, 123, 108, 45, 5);//Window Frames
+            e.Graphics.FillRectangle(Brushes.Moccasin, 143, 92, 5, 38);//Window Frames
+        }
+
+        private void clear_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.LightGray, new Rectangle(0, 0, 200, 200));
+        }
+
+
+        private void btnDrawHouse_Click(object sender, EventArgs e)
+        {
+            //panDraw is the panel that the image is being draw on
+            panDraw.Paint += new PaintEventHandler(house_Paint);
+            panDraw.Invalidate();//Repaints the image
+        }
+
+        private void btnDrawClear_Click(object sender, EventArgs e)
+        {
+            panDraw.Paint += new PaintEventHandler(clear_Paint);
+            panDraw.Refresh(); ;//Repaints the image
+        }
+        
+    }
+}
