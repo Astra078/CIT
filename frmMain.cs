3c89fb24dcb0c15fb44c429524eaeb295b13f096
Astra078 <54424091+Astra078@users.noreply.github.com>
2019-10-07 18:25:10 +1100

    Upload initial files
    Upload initial files for the compound calculator

---
+++ frmMain.cs
@@ -0,0 +1,38 @@
+﻿/**
+*** Author: Conor Lynch CIT185593
+*** Date: 07/10/2019
+*** IDE: Microsoft Visual Studio Enterprise 2017
+*** IDE Version: 15.9.6
+*** Software: Microsoft .NET Framework
+*** Software Version: 4.7.02558
+*** Platform: Microsoft Windows 8.1 64-bit
+*** Platform Version: 6.3.9600 Build 9600
+*** Project: Assessment 2
+*** Objective: Creation of compound calculator that includes nested classes and a Help file
+**/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assessment2NestedClassHelpFile
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
